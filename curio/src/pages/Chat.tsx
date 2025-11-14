d5c956719db7543f7f7686a6b7272a06b07479c7
Ratik Gambhir <ratikgambhir2@gmail.com>
2025-11-13 22:06:50 -0600

    added comment

--- curio/src/pages/Chat.tsx
+++ curio/src/pages/Chat.tsx
@@ -2,9 +2,11 @@ import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { ChatSidebar } from "@/components/chat-sidebar";
 
 const Chat = () => {
+  //TODO: Implement chat functionality with cloudlfare worker
   return (
     <SidebarProvider className="flex w-full h-screen">
       <ChatSidebar />
+
       <SidebarInset className="bg-blue-500 ml-[var(--sidebar-width)] w-[calc(100vw-var(--sidebar-width))]"></SidebarInset>
     </SidebarProvider>
   );
