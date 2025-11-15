469e0ae406460ce8a934f8f15b8095e3bc8fdfad
Ratik Gambhir <ratikgambhir2@gmail.com>
2025-11-15 16:13:58 -0600

    added initial chat components

---
+++ curio/src/components/chat-view.tsx
@@ -0,0 +1,50 @@
+import { useChat } from "@ai-sdk/react";
+import {
+  Conversation,
+  ConversationContent,
+  ConversationEmptyState,
+} from "./ai-elements/conversation";
+import { MessageSquare } from "lucide-react";
+import { Message, MessageContent } from "./ai-elements/message";
+
+const messages: { key: string; value: string; from: "user" | "assistant" }[] = [
+  {
+    key: "1",
+    value: "Hello, how are you?",
+    from: "user",
+  },
+  {
+    key: "2",
+    value: "I'm good, thank you! How can I assist you today?",
+    from: "assistant",
+  },
+];
+
+const mockMessages = [];
+
+export function ChatView({ ...props }) {
+  //const { messages } = useChat();
+  return (
+    <div className="border border-blue-500 size-full mx-auto p-4">
+      <div className="flex flex-col h-full">
+        <Conversation>
+          <ConversationContent>
+            {mockMessages.length > 0 ? (
+              messages.map(({ key, value, from }) => (
+                <Message key={key} from={from}>
+                  <MessageContent>{value}</MessageContent>
+                </Message>
+              ))
+            ) : (
+              <ConversationEmptyState
+                icon={<MessageSquare className="size-12" />}
+                title="Start a conversation"
+                description="Type a message below to begin chatting"
+              />
+            )}
+          </ConversationContent>
+        </Conversation>
+      </div>
+    </div>
+  );
+}
--- curio/src/pages/Chat.tsx
+++ curio/src/pages/Chat.tsx
@@ -1,5 +1,6 @@
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { ChatSidebar } from "@/components/chat-sidebar";
+import { ChatView } from "@/components/chat-view";
 
 const Chat = () => {
   //TODO: Implement chat functionality with cloudlfare worker
@@ -7,7 +8,11 @@ const Chat = () => {
     <SidebarProvider className="flex w-full h-screen">
       <ChatSidebar />
 
-      <SidebarInset className="bg-blue-500 ml-[var(--sidebar-width)] w-[calc(100vw-var(--sidebar-width))]"></SidebarInset>
+      <SidebarInset className="ml-[var(--sidebar-width)] w-[calc(100vw-var(--sidebar-width))]">
+        <ChatView />
+        {/*<div className="border border-blue-500 w-1/2">Left box</div>
+        <div className="border border-yellow-500 w-1/2">Right box</div>*/}
+      </SidebarInset>
     </SidebarProvider>
   );
 };
